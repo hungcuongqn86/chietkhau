4c451540e01d7fb28478368712258810745a7605
cuongnh <hungcuongqn86@gmail.com>
2020-05-22 17:05:15 +0700

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
                     <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                         <div class="features-icons-icon d-flex">
                             <!--<i class="icon-screen-desktop m-auto text-primary"></i>-->
-							<img src="/upload/img/avatar/TAOBAO.png" class="img-fluid">
+							<img src="img/1688/1688_512.png" class="img-fluid">
                         </div>
                         <h3>Fully Responsive</h3>
                         <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
@@ -43,7 +43,7 @@
                     <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                         <div class="features-icons-icon d-flex">
                             <!--<i class="icon-layers m-auto text-primary"></i>-->
-							<img src="/upload/img/avatar/TAOBAO.png" class="img-fluid">
+							<img src="img/1688/1688_512.png" class="img-fluid">
                         </div>
                         <h3>Bootstrap 4 Ready</h3>
                         <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
@@ -53,7 +53,7 @@
                     <div class="features-icons-item mx-auto mb-0 mb-lg-3">
                         <div class="features-icons-icon d-flex">
                             <!--<i class="icon-check m-auto text-primary"></i>-->
-							<img src="/upload/img/avatar/TAOBAO.png" class="img-fluid">
+							<img src="img/1688/1688_512.png" class="img-fluid">
                         </div>
                         <h3>Easy to Use</h3>
                         <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>

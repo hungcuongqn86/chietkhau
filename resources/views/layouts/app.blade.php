e7f4b922e9b3201f246767f115221b1899d73772
cuongnh <hungcuongqn86@gmail.com>
2020-05-22 11:35:49 +0700

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+	<link rel="shortcut icon" type="image/png" href="img/discount16.png"/>
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->

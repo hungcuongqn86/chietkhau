29d366df341452da98c653c2047bc8fa9f0abd34
cuongnh <hungcuongqn86@gmail.com>
2020-05-31 21:00:41 +0700

    taobaosdk

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -36,18 +36,23 @@ class IndexController extends Controller
         $c->appkey = '30074478';
         $c->secretKey = 'c066725d2eb12a5c296b5fc240a210ba';
 
-        /*$req = new \HttpdnsGetRequest;
-
-        $req->putOtherTextParam("name","test");
-        $req->putOtherTextParam("value",0);
-
-        var_dump($c->execute($req));*/
-
-        $req = new \TbkItemInfoGetRequest;
+        /*$req = new \TbkItemInfoGetRequest;
         $req->setNumIids("610903374055");
         $req->setPlatform("1");
         $req->setIp("11.22.33.43");
+        $resp = $c->execute($req);*/
+
+        $req = new \TbkDgOptimusMaterialRequest;
+        $req->setPageSize("20");
+        $req->setAdzoneId("110148550222");
+        $req->setPageNo("1");
+        $req->setMaterialId("13256");
+        $req->setItemId("618962533617");
+        // $req->setQ("610903374055");
+        // $req->setMaterialId("610903374055");
         $resp = $c->execute($req);
+        echo "<pre>";
+        var_dump($resp);
 
 
         /*$client = new Client([
